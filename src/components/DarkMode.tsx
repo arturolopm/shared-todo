fa5d48d76831834cd45b79375aa8b1c989a394a1
R2D2 <carturolopezm@gmail.com>
2023-08-05 16:38:23 -0500

    Work saving theme

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,21 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const DarkMode: React.FC = () => {
-  const [darkMode, setDarkMode] = useState(true)
+  const getThemeInStorage = (): string | null => {
+    return localStorage.getItem('theme')
+  }
+  const theme = !(
+    getThemeInStorage() === 'false' || getThemeInStorage() === null
+  )
+
+  const [darkMode, setDarkMode] = useState(theme || false)
+
+  const handleDarkMode = (): void => {
+    const themeToSave = !darkMode
+    const theme = themeToSave.toString()
+
+    localStorage.setItem('theme', theme)
+    setDarkMode((prev) => !prev)
+  }
 
   const handleFilter = (): void => {
-    setDarkMode((prevInvert) => !prevInvert)
-
     // Get the <html> element and directly modify its style
     const htmlElement = document.querySelector('html')
     if (htmlElement != null) {
-      htmlElement.style.filter = darkMode ? '' : 'invert(1)'
+      htmlElement.style.filter = darkMode ? 'invert(1)' : ''
     }
   }
+  useEffect(() => {
+    handleFilter()
+  }, [darkMode])
 
   return (
     <button
-      onClick={handleFilter}
+      onClick={() => {
+        handleDarkMode()
+      }}
       className='darkbtn'>
       Dark Mode
     </button>
